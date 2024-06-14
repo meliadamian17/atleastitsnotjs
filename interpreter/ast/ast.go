15497256667762b3a7ad682117cbe14af81f7cb8
Damian <meliadamian17@gmail.com>
2024-06-13 21:05:36 -0400

    return statement parsing

--- interpreter/ast/ast.go
+++ interpreter/ast/ast.go
@@ -26,14 +26,23 @@ type LetStatement struct {
 	Value Expression
 }
 
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
 type Identifier struct {
 	Token token.Token
 	Value string
 }
-
-func (ls *LetStatement) statementNode() {}
-
-func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
 func (i *Identifier) expressionNode() {}
 
--- interpreter/parser/parser.go
+++ interpreter/parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/meliadamian17/atleastitsnotjs/interpreter/ast"
 	"github.com/meliadamian17/atleastitsnotjs/interpreter/lexer"
 	"github.com/meliadamian17/atleastitsnotjs/interpreter/token"
@@ -9,17 +11,33 @@ import (
 type Parser struct {
 	l *lexer.Lexer
 
+	errors []string
+
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+
+	p.errors = append(p.errors, msg)
+
 }
 
 func (p *Parser) nextToken() {
@@ -46,6 +64,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -70,6 +90,18 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 
 	return st
 
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	st := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return st
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
